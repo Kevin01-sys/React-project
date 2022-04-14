662508d737da01a3374f1bec57289c1c5b71d006
Kevin Chocano <kevinchocano@gmail.com>
2022-04-14 13:19:22 -0400

    React Course: 5. Properties

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React from "react";
 import logo from './logo.svg';
 import Componente from "./components/Componente";
+import Propiedades from "./components/Propiedades";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <section>
+        <section>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </section>
+        <section>
         <Componente msg="Hola soy un Componente Funcional desde una prop"/>
-      </section>
+        <hr/>
+        <Propiedades
+         cadena="Esto es una cadena de texto" 
+         numero={19} 
+         booleano={true}
+         arreglo={[1,2,3]}
+         objeto={{nombre: "Kevin", correo: "kevinchocano@gmail.com"}}
+         funcion={(num) => num*num}
+         elementoReact={<i>Esto es un elemento React</i>}
+         componenteReact={
+           <Componente msg="Soy un componente pasado como Prop"/>
+         }
+         />
+        </section>
+      </header>
     </div>
   );
 }
---
+++ src/components/Propiedades.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function Propiedades(props){
+    return(
+        <div>
+            <h2>{props.porDefecto}</h2>
+            <ul>
+                <li>{props.cadena}</li>
+                <li>{props.numero}</li>
+                <li>{props.booleano? "Verdadero": "Falso"}</li>
+                <li>{props.arreglo.join(", " )}</li>
+                <li>{`${props.objeto.nombre} + ${props.objeto.correo}`}</li>
+                <li>{props.elementoReact}</li>
+                {/* <li>{props.funcion(2)}</li> */}
+                <li>{props.arreglo.map(props.funcion).join(", ")}</li>
+                <li>{props.componenteReact}</li>
+            </ul>
+        </div>
+    );
+}
+
+Propiedades.defaultProps = {
+    porDefecto: "Las Props",
+}
+
+Propiedades.propTypes = {
+    numero: PropTypes.number.isRequired,
+}
